f3e9d5798a67d4627030bc1fa741ebccbe95d2fb
ANDERSOUNDZ <andersonmikol@live.com>
2022-05-24 16:39:56 -0500

    ingresando proyecto

---
+++ BricoNayonProject/BricoNayonProject/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace BricoNayonProject.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ BricoNayonProject/BricoNayonProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BricoNayonProject.Startup))]
+namespace BricoNayonProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
