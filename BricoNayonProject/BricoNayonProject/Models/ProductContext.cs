d3ac0a00888243dce007fbe2c345da5f47872634
ANDERSOUNDZ <andersonmikol@live.com>
2022-05-25 18:19:30 -0500

    Agregando clases de accesos de datos ProductContext y Clase que inicializa la base de datos ProductDatabaseinitializer

--- BricoNayonProject/BricoNayonProject/Models/ProductContext.cs
+++ BricoNayonProject/BricoNayonProject/Models/ProductContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿using System.Data.Entity;
 namespace BricoNayonProject.Models
 {
-    public class ProductContext
+    public class ProductContext : DbContext
     {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
