d96867472a8557649f88da7b9efb1a14a1601e6f
ANDERSOUNDZ <andersonmikol@live.com>
2022-07-25 17:39:48 -0500

    Modificacion de la funcionalidad de inicio de sesion

--- BricoNayonProject/BricoNayonProject/Account/Login.aspx.cs
+++ BricoNayonProject/BricoNayonProject/Account/Login.aspx.cs
@@ -38,6 +38,10 @@ namespace BricoNayonProject.Account
                 switch (result)
                 {
                     case SignInStatus.Success:
+                        BricoNayonProject.Logic.ShoppingCartActions usersShoppingCart = new BricoNayonProject.Logic.ShoppingCartActions();
+                        String cartId = usersShoppingCart.GetCartId();
+                        usersShoppingCart.MigrateCart(cartId, Email.Text);
+
                         IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
                         break;
                     case SignInStatus.LockedOut:
--- BricoNayonProject/BricoNayonProject/Account/Register.aspx.cs
+++ BricoNayonProject/BricoNayonProject/Account/Register.aspx.cs
@@ -25,6 +25,13 @@ namespace BricoNayonProject.Account
                 //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
 
                 signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
+
+                using (BricoNayonProject.Logic.ShoppingCartActions usersShoppingCart = new BricoNayonProject.Logic.ShoppingCartActions())
+                {
+                    String cartId = usersShoppingCart.GetCartId();
+                    usersShoppingCart.MigrateCart(cartId, user.Id);
+                }
+
                 IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
             }
             else 
